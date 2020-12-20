05bdcb4a266856df759e02f5db53d91a257903c6
Stevie <xxlennygamerxx@gmail.com>
2020-12-19 22:26:38 -0800

    Create WarModeGlobalNPC.cs

---
+++ Content/NPCs/WarMode/WarModeGlobalNPC.cs
@@ -0,0 +1,20 @@
+﻿using FanIdeasMod.Common.WorldModifications.WarMode;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FanIdeasMod.Content.NPCs.WarMode
+{
+    public class WarModeGlobalNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            switch (npc.type)
+            {
+                case NPCID.MoonLordCore:
+                    WarModeWorld.IsWarMode = true;
+                    break;
+            }
+        }
+    }
+}
